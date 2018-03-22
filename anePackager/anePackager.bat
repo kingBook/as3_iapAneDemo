206c9a3e5c61ef27e30254438254d4f25ae2b0c0
kingBook <412126604@qq.com>
2018-03-22 18:42:35 +0800

    update 2018-03-22 18:42

--- anePackager/anePackager.bat
+++ anePackager/anePackager.bat
@@ -1,2 +1,8 @@
-::需要先将flex_sdk_4.6\bin目录添加到Path系统环境变量才可以运行
+::需要先将FLEX_SDK路径D:\flex_sdk_4.6\bin添加到Path系统环境变量,或设置FLEX_SDK_HOME路径，才可以运行
+
+::方法一
 adt -package -target ane flashane.ane extension.xml -swc aneswc.swc -platform iPhone-ARM library.swf ioslib.a -platformoptions platformoptions.xml
+
+::方法二
+::set FLEX_SDK_HOME=D:\flex_sdk_4.6\bin
+::"%FLEX_SDK_HOME%\adt" -package -target ane flashane.ane extension.xml -swc aneswc.swc -platform iPhone-ARM library.swf ioslib.a -platformoptions platformoptions.xml
